0779ac91902c5f3f68e6506aa2f221ef3017f2bc
devallever <devallever@163.com>
2020-11-01 15:31:07 +0800

    NONE-CODE: 屏蔽dao

--- app/src/main/java/org/xm/app/translator/opensource/mvvm/repository/db/AppDatabase.kt
+++ app/src/main/java/org/xm/app/translator/opensource/mvvm/repository/db/AppDatabase.kt
@@ -1,35 +1,35 @@
-package org.xm.app.translator.opensource.mvvm.repository.db
-
-import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import org.xm.app.translator.opensource.mvvm.repository.db.dao.HistoryDao
-import org.xm.app.translator.opensource.mvvm.repository.db.entity.History
-
-@Database(
-    version = 1,
-    entities = [History::class]
-)
-abstract class AppDatabase : RoomDatabase() {
-
-    abstract fun historyDao(): HistoryDao
-
-    companion object {
-        private var mInstance: AppDatabase? = null
-        fun getDatabase(context: Context): AppDatabase {
-            mInstance?.let {
-                return it
-            }
-
-            return Room.databaseBuilder(
-                context.applicationContext,
-                AppDatabase::class.java,
-                "TranslatorDatabase"
-            )
-                .build().apply {
-                    mInstance = this
-                }
-        }
-    }
-}
+//package org.xm.app.translator.opensource.mvvm.repository.db
+//
+//import android.content.Context
+//import androidx.room.Database
+//import androidx.room.Room
+//import androidx.room.RoomDatabase
+//import org.xm.app.translator.opensource.mvvm.repository.db.dao.HistoryDao
+//import org.xm.app.translator.opensource.mvvm.repository.db.entity.History
+//
+//@Database(
+//    version = 1,
+//    entities = [History::class]
+//)
+//abstract class AppDatabase : RoomDatabase() {
+//
+//    abstract fun historyDao(): HistoryDao
+//
+//    companion object {
+//        private var mInstance: AppDatabase? = null
+//        fun getDatabase(context: Context): AppDatabase {
+//            mInstance?.let {
+//                return it
+//            }
+//
+//            return Room.databaseBuilder(
+//                context.applicationContext,
+//                AppDatabase::class.java,
+//                "TranslatorDatabase"
+//            )
+//                .build().apply {
+//                    mInstance = this
+//                }
+//        }
+//    }
+//}
--- app/src/main/java/org/xm/app/translator/opensource/mvvm/repository/db/dao/HistoryDao.kt
+++ app/src/main/java/org/xm/app/translator/opensource/mvvm/repository/db/dao/HistoryDao.kt
@@ -1,37 +1,37 @@
-package org.xm.app.translator.opensource.mvvm.repository.db.dao
-
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
-import org.xm.app.translator.opensource.mvvm.repository.db.entity.History
-import retrofit2.http.DELETE
-
-@Dao
-interface HistoryDao {
-
-    @Insert
-    fun insertHistory(history: History): Long
-
-    @DELETE
-    fun deleteHistory(history: History)
-
-    @Update()
-    fun updateHistory(history: History)
-
-    @Query("select * from History where srcText = :content and sl = :sl and tl = :tl")
-    fun getHistory(content: String, sl: String, tl: String): MutableList<History>
-
-    @Query("select * from History order by time desc")
-    fun getAllHistory(): MutableList<History>
-
-    @Query("select * from History where sl = :sl")
-    fun getHistoryBySl(sl: String): MutableList<History>
-
-    @Query("select * from History where tl = :tl")
-    fun getHistoryByTl(tl: String): MutableList<History>
-
-    @Query("select * from History where sl = :sl and tl = :tl")
-    fun getHistoryBySlAndTl(sl: String, tl: String): MutableList<History>
-
-}
+//package org.xm.app.translator.opensource.mvvm.repository.db.dao
+//
+//import androidx.room.Dao
+//import androidx.room.Insert
+//import androidx.room.Query
+//import androidx.room.Update
+//import org.xm.app.translator.opensource.mvvm.repository.db.entity.History
+//import retrofit2.http.DELETE
+//
+//@Dao
+//interface HistoryDao {
+//
+//    @Insert
+//    fun insertHistory(history: History): Long
+//
+//    @DELETE
+//    fun deleteHistory(history: History)
+//
+//    @Update()
+//    fun updateHistory(history: History)
+//
+//    @Query("select * from History where srcText = :content and sl = :sl and tl = :tl")
+//    fun getHistory(content: String, sl: String, tl: String): MutableList<History>
+//
+//    @Query("select * from History order by time desc")
+//    fun getAllHistory(): MutableList<History>
+//
+//    @Query("select * from History where sl = :sl")
+//    fun getHistoryBySl(sl: String): MutableList<History>
+//
+//    @Query("select * from History where tl = :tl")
+//    fun getHistoryByTl(tl: String): MutableList<History>
+//
+//    @Query("select * from History where sl = :sl and tl = :tl")
+//    fun getHistoryBySlAndTl(sl: String, tl: String): MutableList<History>
+//
+//}
